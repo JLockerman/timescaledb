f00dff285861a752f6dd8aebeb6deae05ad0e353
Joshua Lockerman <>
2018-07-27 14:54:04 -0400

    more tests

--- test/sql/cluster.sql
+++ test/sql/cluster.sql
@@ -12,19 +12,24 @@ INSERT INTO cluster_test VALUES ('2017-01-20T09:00:01', 23.4, 1),
 -- after each test we ensure bitmap scans work
 BEGIN;
     SET LOCAL enable_seqscan=false;
+    SET LOCAL enable_indexscan=false;
+    SET LOCAL enable_bitmapscan=true;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
     SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
 COMMIT;
 
 -- after each test we ensure index scans work
 BEGIN;
-    SET LOCAL enable_indexscan=false;
+    SET LOCAL enable_seqscan=false;
+    SET LOCAL enable_indexscan=true;
+    SET LOCAL enable_bitmapscan=false;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
     SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
 COMMIT;
 
 -- after each test we ensure sequntial scans work
 BEGIN;
+    SET LOCAL enable_seqscan=true;
     SET LOCAL enable_indexscan=false;
     SET LOCAL enable_bitmapscan=false;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
@@ -34,19 +39,27 @@ COMMIT;
 -- Run cluster
 CLUSTER VERBOSE cluster_test USING cluster_test_time_idx;
 
+-- after each test we ensure bitmap scans work
 BEGIN;
     SET LOCAL enable_seqscan=false;
-    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
-    SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
-COMMIT;
-
-BEGIN;
     SET LOCAL enable_indexscan=false;
+    SET LOCAL enable_bitmapscan=true;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
     SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
 COMMIT;
 
+-- after each test we ensure index scans work
 BEGIN;
+    SET LOCAL enable_seqscan=false;
+    SET LOCAL enable_indexscan=true;
+    SET LOCAL enable_bitmapscan=false;
+    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
+    SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
+COMMIT;
+
+-- after each test we ensure sequntial scans work
+BEGIN;
+    SET LOCAL enable_seqscan=true;
     SET LOCAL enable_indexscan=false;
     SET LOCAL enable_bitmapscan=false;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
@@ -64,19 +77,27 @@ WHERE indisclustered = true;
 -- Recluster just our table
 CLUSTER VERBOSE cluster_test;
 
+-- after each test we ensure bitmap scans work
 BEGIN;
     SET LOCAL enable_seqscan=false;
-    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
-    SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
-COMMIT;
-
-BEGIN;
     SET LOCAL enable_indexscan=false;
+    SET LOCAL enable_bitmapscan=true;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
     SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
 COMMIT;
 
+-- after each test we ensure index scans work
 BEGIN;
+    SET LOCAL enable_seqscan=false;
+    SET LOCAL enable_indexscan=true;
+    SET LOCAL enable_bitmapscan=false;
+    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
+    SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
+COMMIT;
+
+-- after each test we ensure sequntial scans work
+BEGIN;
+    SET LOCAL enable_seqscan=true;
     SET LOCAL enable_indexscan=false;
     SET LOCAL enable_bitmapscan=false;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
@@ -91,19 +112,27 @@ WHERE indisclustered = true;
 -- Recluster all tables (although will only be our test table)
 CLUSTER VERBOSE;
 
+-- after each test we ensure bitmap scans work
 BEGIN;
     SET LOCAL enable_seqscan=false;
-    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
-    SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
-COMMIT;
-
-BEGIN;
     SET LOCAL enable_indexscan=false;
+    SET LOCAL enable_bitmapscan=true;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
     SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
 COMMIT;
 
+-- after each test we ensure index scans work
 BEGIN;
+    SET LOCAL enable_seqscan=false;
+    SET LOCAL enable_indexscan=true;
+    SET LOCAL enable_bitmapscan=false;
+    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
+    SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
+COMMIT;
+
+-- after each test we ensure sequntial scans work
+BEGIN;
+    SET LOCAL enable_seqscan=true;
     SET LOCAL enable_indexscan=false;
     SET LOCAL enable_bitmapscan=false;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
@@ -115,19 +144,27 @@ CREATE INDEX ON cluster_test (time, location);
 
 CLUSTER VERBOSE cluster_test USING cluster_test_time_location_idx;
 
+-- after each test we ensure bitmap scans work
 BEGIN;
     SET LOCAL enable_seqscan=false;
-    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
-    SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
-COMMIT;
-
-BEGIN;
     SET LOCAL enable_indexscan=false;
+    SET LOCAL enable_bitmapscan=true;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
     SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
 COMMIT;
 
+-- after each test we ensure index scans work
 BEGIN;
+    SET LOCAL enable_seqscan=false;
+    SET LOCAL enable_indexscan=true;
+    SET LOCAL enable_bitmapscan=false;
+    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
+    SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
+COMMIT;
+
+-- after each test we ensure sequntial scans work
+BEGIN;
+    SET LOCAL enable_seqscan=true;
     SET LOCAL enable_indexscan=false;
     SET LOCAL enable_bitmapscan=false;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
@@ -148,19 +185,27 @@ CLUSTER VERBOSE _timescaledb_internal._hyper_1_2_chunk using cluster_test_time_i
 -- CLUSTER a chunk directly with an chunk index
 CLUSTER VERBOSE _timescaledb_internal._hyper_1_2_chunk using _hyper_1_2_chunk_cluster_test_time_idx;
 
+-- after each test we ensure bitmap scans work
 BEGIN;
     SET LOCAL enable_seqscan=false;
-    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
-    SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
-COMMIT;
-
-BEGIN;
     SET LOCAL enable_indexscan=false;
+    SET LOCAL enable_bitmapscan=true;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
     SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
 COMMIT;
 
+-- after each test we ensure index scans work
 BEGIN;
+    SET LOCAL enable_seqscan=false;
+    SET LOCAL enable_indexscan=true;
+    SET LOCAL enable_bitmapscan=false;
+    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
+    SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
+COMMIT;
+
+-- after each test we ensure sequntial scans work
+BEGIN;
+    SET LOCAL enable_seqscan=true;
     SET LOCAL enable_indexscan=false;
     SET LOCAL enable_bitmapscan=false;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
@@ -177,19 +222,27 @@ UPDATE pg_settings SET setting = 'native' WHERE name = 'timescaledb.cluster_meth
 CLUSTER VERBOSE cluster_test USING cluster_test_time_location_idx;
 CLUSTER VERBOSE _timescaledb_internal._hyper_1_2_chunk;
 
+-- after each test we ensure bitmap scans work
 BEGIN;
     SET LOCAL enable_seqscan=false;
-    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
-    SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
-COMMIT;
-
-BEGIN;
     SET LOCAL enable_indexscan=false;
+    SET LOCAL enable_bitmapscan=true;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
     SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
 COMMIT;
 
+-- after each test we ensure index scans work
 BEGIN;
+    SET LOCAL enable_seqscan=false;
+    SET LOCAL enable_indexscan=true;
+    SET LOCAL enable_bitmapscan=false;
+    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
+    SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
+COMMIT;
+
+-- after each test we ensure sequntial scans work
+BEGIN;
+    SET LOCAL enable_seqscan=true;
     SET LOCAL enable_indexscan=false;
     SET LOCAL enable_bitmapscan=false;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
@@ -203,19 +256,27 @@ UPDATE pg_settings SET setting = 'read_optimized' WHERE name = 'timescaledb.clus
 CLUSTER VERBOSE cluster_test USING cluster_test_time_location_idx;
 CLUSTER VERBOSE _timescaledb_internal._hyper_1_2_chunk;
 
+-- after each test we ensure bitmap scans work
 BEGIN;
     SET LOCAL enable_seqscan=false;
-    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
-    SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
-COMMIT;
-
-BEGIN;
     SET LOCAL enable_indexscan=false;
+    SET LOCAL enable_bitmapscan=true;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
     SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
 COMMIT;
 
+-- after each test we ensure index scans work
 BEGIN;
+    SET LOCAL enable_seqscan=false;
+    SET LOCAL enable_indexscan=true;
+    SET LOCAL enable_bitmapscan=false;
+    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
+    SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
+COMMIT;
+
+-- after each test we ensure sequntial scans work
+BEGIN;
+    SET LOCAL enable_seqscan=true;
     SET LOCAL enable_indexscan=false;
     SET LOCAL enable_bitmapscan=false;
     EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2017-01-21T09:00:01';
@@ -233,16 +294,47 @@ SELECT * FROM create_hypertable('toast_test', 'time');
 INSERT INTO toast_test VALUES('2004-10-19 10:23:54', $$
 this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k. this must be over 2k.
 $$);
+
 SELECT * FROM toast_test;
 
-CLUSTER VERBOSE toast_test USING cluster_test_time_idx;
+CLUSTER VERBOSE toast_test USING toast_test_time_idx;
 SELECT * FROM toast_test;
 
--- force CLUSTER to vaccume the toast_table
+-- force CLUSTER to vaccum the toast_table
 ALTER TABLE toast_test DROP COLUMN value;
 SELECT * FROM toast_test;
 CLUSTER VERBOSE toast_test USING cluster_test_time_idx;
 SELECT * FROM toast_test;
+
+-- after each test we ensure bitmap scans work
+BEGIN;
+    SET LOCAL enable_seqscan=false;
+    SET LOCAL enable_indexscan=false;
+    SET LOCAL enable_bitmapscan=true;
+    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2004-10-19 10:23:54';
+    SELECT * FROM toast_test WHERE time='2004-10-19 10:23:54';
+COMMIT;
+
+-- after each test we ensure index scans work
+BEGIN;
+    SET LOCAL enable_seqscan=false;
+    SET LOCAL enable_indexscan=true;
+    SET LOCAL enable_bitmapscan=false;
+    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2004-10-19 10:23:54';
+    SELECT * FROM toast_test WHERE time='2004-10-19 10:23:54';
+COMMIT;
+
+-- after each test we ensure sequntial scans work
+BEGIN;
+    SET LOCAL enable_seqscan=true;
+    SET LOCAL enable_indexscan=false;
+    SET LOCAL enable_bitmapscan=false;
+    EXPLAIN (costs off) SELECT * FROM cluster_test WHERE time='2004-10-19 10:23:54';
+    SELECT * FROM toast_test WHERE time='2004-10-19 10:23:54';
+COMMIT;
+
+-- if we someone elses index error
+CLUSTER VERBOSE toast_test USING cluster_test_time_idx;
 
 --check the setting of cluster indexes on hypertables and chunks
 ALTER TABLE cluster_test CLUSTER ON cluster_test_time_idx;
