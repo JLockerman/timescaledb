68bbb0a438ca1d16f9d9523731637473a2b59d32
Joshua Lockerman <josh@timescale.com>
2019-02-04 13:55:32 -0500

    Partialize function works for all postgres supplied aggregates
    We currently disallow statements with some partialized and some non-partialized
    aggregates.

--- src/plan_partialize.c
+++ src/plan_partialize.c
@@ -23,90 +23,81 @@ TS_FUNCTION_INFO_V1(ts_partialize);
 TSDLLEXPORT Datum
 ts_partialize(PG_FUNCTION_ARGS)
 {
-    Datum arg;
-    Oid arg_type;
-    Oid send_fn;
-    bool type_is_varlena;
-    if (PG_ARGISNULL(0))
-        PG_RETURN_NULL();
+	Datum		arg;
+	Oid			arg_type;
+	Oid			send_fn;
+	bool		type_is_varlena;
 
-    arg = PG_GETARG_DATUM(0);
-    arg_type = get_fn_expr_argtype(fcinfo->flinfo, 0);
+	if (PG_ARGISNULL(0))
+		PG_RETURN_NULL();
 
-    if (arg_type == BYTEAOID)
-        PG_RETURN_DATUM(arg);
+	arg = PG_GETARG_DATUM(0);
+	arg_type = get_fn_expr_argtype(fcinfo->flinfo, 0);
 
-    getTypeBinaryOutputInfo(arg_type, &send_fn, &type_is_varlena);
+	if (arg_type == BYTEAOID)
+		PG_RETURN_DATUM(arg);
 
+	getTypeBinaryOutputInfo(arg_type, &send_fn, &type_is_varlena);
 
-    {
-        bytea *binary = OidSendFunctionCall(send_fn, arg);
-        StringInfo serialized = makeStringInfo();
-        Oid recv_fn, typIOParam, out_fn;
-        getTypeOutputInfo(arg_type, &out_fn, &type_is_varlena);
-        getTypeBinaryInputInfo(arg_type, &recv_fn, &typIOParam);
-
-        appendBinaryStringInfo(serialized, VARDATA_ANY(binary), VARSIZE_ANY_EXHDR(binary));
-
-        elog(WARNING, "output: %s", OidOutputFunctionCall(out_fn, arg));
-
-        Datum received = OidReceiveFunctionCall(recv_fn, serialized, typIOParam, 0);
-        elog(WARNING, "ioParam %d, arg %d", typIOParam, arg_type);
-        elog(WARNING, "received: %s", OidOutputFunctionCall(out_fn, received));
-    }
-
-    PG_RETURN_BYTEA_P(OidSendFunctionCall(send_fn, arg));
+	PG_RETURN_BYTEA_P(OidSendFunctionCall(send_fn, arg));
 
 }
 
-///////////////////////////////////////
+/* ///////////////////////////////////// */
 
 typedef struct PartializeWalkerState
 {
-    bool found_partialize;
-    bool looking_for_agg;
+	bool		found_partialize;
+	bool		looking_for_agg;
 } PartializeWalkerState;
 
 static bool
 partialize_function_call_walker(Node *node, PartializeWalkerState *state)
 {
-    if (node == NULL)
-        return false;
+	if (node == NULL)
+		return false;
 
-    if (state->looking_for_agg)
-    {
-        Aggref *agg_ref;
-        if (!IsA(node, Aggref))
-            elog(ERROR, "The input to partialize must be an aggregate");
+	/*
+	 * If the last node we saw was partialize, the next one must be aggregate
+	 * we're partializing
+	 */
+	if (state->looking_for_agg)
+	{
+		Aggref	   *agg_ref;
 
-        elog(WARNING, "aggref");
-        agg_ref = castNode(Aggref, node);
-        agg_ref->aggsplit = AGGSPLIT_INITIAL_SERIAL;
-        if (agg_ref->aggtranstype == INTERNALOID && DO_AGGSPLIT_SERIALIZE(AGGSPLIT_INITIAL_SERIAL))
-            agg_ref->aggtype = BYTEAOID;
-        else
-            agg_ref->aggtype = agg_ref->aggtranstype;
+		if (!IsA(node, Aggref))
+			elog(ERROR, "The input to partialize must be an aggregate");
 
-        state->looking_for_agg = false;
-    }
-    else if (IsA(node, FuncExpr) && strncmp(get_func_name(castNode(FuncExpr, node)->funcid), "partialize", NAMEDATALEN) == 0)
-    {
-        elog(WARNING, "partialize");
-        state->found_partialize = true;
-        state->looking_for_agg = true;
-    }
+		agg_ref = castNode(Aggref, node);
+		agg_ref->aggsplit = AGGSPLIT_INITIAL_SERIAL;
+		if (agg_ref->aggtranstype == INTERNALOID && DO_AGGSPLIT_SERIALIZE(AGGSPLIT_INITIAL_SERIAL))
+			agg_ref->aggtype = BYTEAOID;
+		else
+			agg_ref->aggtype = agg_ref->aggtranstype;
+
+		state->looking_for_agg = false;
+	}
+	else if (IsA(node, FuncExpr) &&strncmp(get_func_name(castNode(FuncExpr, node)->funcid), "partialize", NAMEDATALEN) == 0)
+	{
+		state->found_partialize = true;
+		state->looking_for_agg = true;
+	}
 
 	return expression_tree_walker((Node *) node, partialize_function_call_walker, state);
 }
 
+/* We currently cannot handle cases like
+ *     SELECT sum(i), partialize(sum(i)) ...
+ * instead we use this function to ensure that if any of the aggregates in a staetment are partialized, all of them are
+ */
 static bool
 ensure_only_partials(Node *node, void *state)
 {
-    if (node == NULL)
-        return false;
+	if (node == NULL)
+		return false;
 
-    if (IsA(node, Aggref) && castNode(Aggref, node)->aggsplit != AGGSPLIT_INITIAL_SERIAL)
-            elog(ERROR, "Cannot mix partialized and non-partialized aggregates in the same statement");
+	if (IsA(node, Aggref) &&castNode(Aggref, node)->aggsplit != AGGSPLIT_INITIAL_SERIAL)
+		elog(ERROR, "Cannot mix partialized and non-partialized aggregates in the same statement");
 
 	return expression_tree_walker((Node *) node, ensure_only_partials, state);
 }
@@ -115,39 +106,35 @@ void
 plan_add_partialize(PlannerInfo *root, RelOptInfo *input_rel, RelOptInfo *output_rel)
 {
 	Query	   *parse = root->parse;
-    PartializeWalkerState state = {
-        .found_partialize = false,
-        .looking_for_agg = false,
-    };
-    ListCell *lc;
+	PartializeWalkerState state = {
+		.found_partialize = false,
+		.looking_for_agg = false,
+	};
+	ListCell   *lc;
 
-    if (CMD_SELECT != parse->commandType)
+	if (CMD_SELECT != parse->commandType)
 		return;
 
-    expression_tree_walker((Node *) parse->targetList, partialize_function_call_walker, &state);
+	expression_tree_walker((Node *) parse->targetList, partialize_function_call_walker, &state);
 
-    if (state.found_partialize)
-    {
-        expression_tree_walker((Node *) parse->targetList, ensure_only_partials,NULL);
+	if (state.found_partialize)
+	{
+		expression_tree_walker((Node *) parse->targetList, ensure_only_partials, NULL);
 
-        foreach(lc, input_rel->pathlist)
+		foreach(lc, input_rel->pathlist)
 		{
-            Path *path = lfirst(lc);
-            if (IsA(path, AggPath)) {
-                elog(WARNING, "in path");
-                ((AggPath *)path)->aggsplit = AGGSPLIT_INITIAL_SERIAL;
-            }
+			Path	   *path = lfirst(lc);
+
+			if (IsA(path, AggPath))
+				((AggPath *) path)->aggsplit = AGGSPLIT_INITIAL_SERIAL;
 		}
 
-        foreach(lc, output_rel->pathlist)
+		foreach(lc, output_rel->pathlist)
 		{
-            Path *path = lfirst(lc);
-            if (IsA(path, AggPath)) {
-                elog(WARNING, "out path");
-                ((AggPath *)path)->aggsplit = AGGSPLIT_INITIAL_SERIAL;
-            }
-		}
+			Path	   *path = lfirst(lc);
 
-        elog(WARNING, "");
-    }
+			if (IsA(path, AggPath))
+				((AggPath *) path)->aggsplit = AGGSPLIT_INITIAL_SERIAL;
+		}
+	}
 }
